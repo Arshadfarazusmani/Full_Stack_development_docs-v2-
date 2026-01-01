59a68f84fc01e6991ff92209fff86b9a1878c88e
Arshad Faraz <arshadfarazusmani@gmail.com>
2026-01-01 16:55:54 +0530

    add component based architecture project to codebase

---
+++ 03_Reactjs/03_reactComponents/src/App.jsx
@@ -0,0 +1,28 @@
+import './App.css'
+import './index.css'
+import Card from './components/Card.jsx'
+import Navbar from './components/Navbar.jsx'
+
+function App() {
+  
+
+  return (
+    <>
+    <Navbar />
+    <Card />
+    <Card />
+    <Card />
+    <Card />
+    <Card />
+    <Card />
+    <Card />
+    <Card />
+    <Card />
+    <Card />
+    <Card />
+     
+    </>
+  )
+}
+
+export default App
---
+++ 03_Reactjs/03_reactComponents/src/components/Card.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Card() {
+  return (
+    <div>
+        <div className="card">
+      <h1>
+        Arshad Faraz
+      </h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo, ratione?</p>
+     </div>
+      
+    </div>
+  )
+}
+
+export default Card
